dce528ff9d3c16aac36acb752f079f0156a82a71
bin.zhang02 <bin.zhang02@weimob.com>
2022-03-28 15:19:46 +0800

    type: style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 function component() {
-  console.log('faith=============lanch-webpack');
-  let element = document.createElement('div');
+  console.log("faith=============lanch-webpack");
+  let element = document.createElement("div");
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  element.innerHTML = 'hello webpack-1'
+  element.innerHTML = "hello webpack-1";
 
   return element;
 }
